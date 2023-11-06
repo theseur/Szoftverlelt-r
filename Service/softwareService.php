f31da68526d0cc4584660e6766a705d4842fbbde
Norbert <balogh.norbert92@gmail.com>
2023-11-06 11:18:49 +0100

    fix import error

--- Service/softwareService.php
+++ Service/softwareService.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once './../Model/db.php';
-require_once './../Model/szoftver.php';
-require_once './../Service/softwareService.php';
+require_once __DIR__ . '/../Model/db.php';
+require_once __DIR__ . '/../Model/szoftver.php';
+require_once __DIR__ . '/../Service/softwareService.php';
 
 class SoftwareService
 {
@@ -16,8 +16,7 @@ class SoftwareService
 
     public function listAllSoftware()
     {
-        $query = "SELECT * FROM szoftver
-        where deactivate=0";
+        $query = "SELECT * FROM szoftver where (deactivate=0 OR deactivate = NULL)";
         $softwares = [];
         foreach ($this->dbModel->findAll($query) as $row) {
             array_push($softwares, new Szoftver($row));
